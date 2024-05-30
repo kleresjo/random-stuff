adc5c162719e6cff23c60f808bf196295342c754
nili <145498352+niloofar-shahsavar@users.noreply.github.com>
2024-05-30 19:59:15 +0200

    Created product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import ProductPage from "./pages/ProductPage";
-import './App.css'
+import "./App.css";
+import { CartProvider } from "./components/CartContext";
 
 function App() {
-
   return (
-   <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/kassa" element={<CartPage />} />
-    <Route path="/betalning" element={<CheckoutPage />} />
-    <Route path="/produkt/:id" element={<ProductPage />} />
-    </Routes>
-    </BrowserRouter>
-  )
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/kassa" element={<CartPage />} />
+          <Route path="/betalning" element={<CheckoutPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 const CartPage = () => {
     return (
         <div>
-            
+            Hello from CartPage
         </div>
     )
 }
