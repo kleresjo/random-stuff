6763b5a6a0c54ed4cf51735e4cce7a9a9364a3a9
Christin <christin25@hotmail.se>
2024-06-03 23:40:16 +0200

    kund korgen går att lägga in och ta bort i, priset räknas och man kan navigera sig från och till.(la ochså till en saknadd'/'i naven på cart

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../components/navbar/Navbar";
+import { CartContext } from "../context/CartContext";
+import { Link, useParams } from "react-router-dom";
 
 const CartPage = () => {
+  const { cart, removeFromCart } = useContext(CartContext);
+
+  const getCartTotal = (product) => {
+    return product.reduce((accumulator, object) => {
+      return accumulator + object.price;
+    }, 0);
+  };
+
   return (
     <>
       <Navbar />
-      <div>Hello from CartPage</div>
+      <div>
+        <h2>CartPage</h2>
+        <div>
+          <button>
+            <Link to={"/"}>Handla mer</Link>
+          </button>
+          <button>
+            <Link to={"/payment"}>Gå till kassan</Link>
+          </button>
+        </div>
+      </div>
+      <p>Total Price: {getCartTotal(cart)} kr</p>
+      <ul>
+        {cart.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title}></img>
+            <p>{product.title}</p>
+            <p> {product.price} :-</p>
+
+            <button onClick={() => removeFromCart(product.id)}>radera</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
