99bc52acb40af91428102d0aee09a8a61bb704c1
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2024-06-07 11:46:56 +0200

    small fixes

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { IoMdCart } from "react-icons/io";
+// import { IoMdCart } from "react-icons/io";
 import "../styling/ProductsAPI.css";
 
 function ProductsList() {
