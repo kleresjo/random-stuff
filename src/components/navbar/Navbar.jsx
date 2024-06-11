041e7092c0be6867ff2abaae9a935124fb620e36
sofiaroos <145346920+Mrsroozen@users.noreply.github.com>
2024-06-11 13:51:35 +0200

    added more css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styling/Navbar.css";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import { PiShoppingCart } from "react-icons/pi";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+
+    console.log("öppnad");
+  };
+
   return (
-    <div className="navbar-container">
-      <Link to="/">
-        <img src={logo} alt="" className="logo" />
-      </Link>
-
-      <div className="nav-links-container">
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/about" className="nav-link">
-          About us
-        </Link>
-        <Link to="/contact" className="nav-link">
-          Contact
-        </Link>
-      </div>
-      <div className="nav-shopping-cart-container">
-        <Link to="/cart" className="nav-link">
-          Shopping cart
+    <div className="navbar-container-highest">
+      <div className="navbar-container">
+        <Link to="/">
+          <img src={logo} alt="" className="logo" />
         </Link>
 
-        <PiShoppingCart className="shopping-cart" />
+        <div className={"nav-links-container ${menuOpen ? 'open' : ''}"}>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/about" className="nav-link">
+            About us
+          </Link>
+          <Link to="/contact" className="nav-link">
+            Contact
+          </Link>
+        </div>
+        <div className="nav-shopping-cart-container">
+          <Link to="/cart" className="nav-link">
+            Shopping cart
+          </Link>
+          <PiShoppingCart className="shopping-cart" />
+          <RxHamburgerMenu
+            className="hamburgermenu"
+            onClick={handleMenuToggle}
+          />
+        </div>
       </div>
     </div>
   );
