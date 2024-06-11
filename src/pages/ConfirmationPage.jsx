b1550dd8bf8f8be1b4a1eef63b7c628c5d1dd779
sofiaroos <145346920+Mrsroozen@users.noreply.github.com>
2024-06-11 11:54:04 +0200

    more css

--- src/pages/ConfirmationPage.jsx
+++ src/pages/ConfirmationPage.jsx
@@ -14,13 +14,15 @@ const ConfirmationPage = () => {
       <div className="confirmation-page-container">
         <div>
           <img src={checkcircle} alt="" />
-          <p>Thank you for shopping with RandomStuff</p>
-          <h1>Order Confirmation!</h1>
-          <p>
+          <p className="confirmation-page-text">
+            Thank you for shopping with RandomStuff
+          </p>
+          <h1 className="confirmation-page-h1">Order Confirmation!</h1>
+          <p className="confirmation-page-text">
             We have received your order and a receipt has been sent to your
             email address.
           </p>
-          <p>Ordernumber: 678319782</p>
+          <p className="confirmation-page-text">Ordernumber: 678319782</p>
         </div>
         <div>
           <button onClick={handleShoppingButton} className="total-price-button">
