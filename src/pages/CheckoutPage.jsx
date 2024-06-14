45fcb89ad03295cbd6676decc92127eb4c38c02d
nili <145498352+niloofar-shahsavar@users.noreply.github.com>
2024-06-14 14:14:51 +0200

    Added "Back to cart" btn on checkoutPage, fixed product image size

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -4,10 +4,22 @@ import MyInformation from "../components/checkout/MyInformation";
 import PaymentInformation from "../components/checkout/PaymentInformation";
 import TotalPrice from "../components/checkout/TotalPrice";
 import "../styling/CheckoutPage.css";
+import { AiOutlineLeft } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
-const CheckoutPage = () => {
+const CheckoutPage = () => { 
+const navigate = useNavigate();
   return (
     <div className="checkoutpage-first-container">
+            <div className="back-to-cart-navigation">
+          <div
+            className="back-to-cart-button"
+            onClick={() => navigate("/cart")}
+          >
+            <AiOutlineLeft size={24} />
+            <span className="back-to-cart-page-text">Back to cart</span>
+          </div>
+        </div>
       <h1 className="checkoutpage-h1">Checkout</h1>
       <div className="checkoutpage-container">
         <div className="checkoutpage-container-left">
