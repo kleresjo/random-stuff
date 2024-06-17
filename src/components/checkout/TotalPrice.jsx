d7837b2c067c38058a5dc913de6865ed67d2f2d7
Julia Kleresjö <92084007+kleresjo@users.noreply.github.com>
2024-06-17 10:03:05 +0200

    fixed pricing in checkoutpage

--- src/components/checkout/TotalPrice.jsx
+++ src/components/checkout/TotalPrice.jsx
@@ -12,6 +12,10 @@ const TotalPrice = () => {
     }, 0);
   };
 
+   const formatPricing = (price) => {
+    return price.toFixed(2);
+  };
+
 
   const getTotalPriceWithDelivery = (product) => {
     return product.reduce((accumulator, object) => {
@@ -31,7 +35,7 @@ const TotalPrice = () => {
         <h3 className="total-price-h3">Summary</h3>
         <div className="total-price-row-container">
           <p className="total-price-text">Order value: </p>
-          <p className="total-price-text">{getTotalPrice(cart)} SEK</p>
+          <p className="total-price-text"> {formatPricing(getTotalPriceWithDelivery(cart))} SEK</p>
         </div>
 
         <div className="total-price-row-container">
@@ -42,7 +46,7 @@ const TotalPrice = () => {
         <div className="total-price-row-container">
           <h2 className="total-price-h2">Total Price: </h2>
           <h2 className="total-price-h2">
-            {getTotalPriceWithDelivery(cart)} SEK
+            {formatPricing(getTotalPriceWithDelivery(cart))} SEK
           </h2>
         </div>
       </div>
